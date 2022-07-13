6784f50749ecc6d1af3c00069c93a88da3aa9ed7
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-07-13 19:59:43 +0100

    Version 0.02 - Added additional MOS commands, bug fixes

--- src/mos.c
+++ src/mos.c
@@ -2,10 +2,12 @@
  * Title:			AGON MOS - MOS code
  * Author:			Dean Belfield
  * Created:			10/07/2022
- * Last Updated:	11/07/2022
+ * Last Updated:	13/07/2022
  * 
  * Modinfo:
  * 11/07/2022:		Added mos_cmdDIR, mos_cmdLOAD, removed mos_cmdBYE
+ * 12/07/2022:		Added mos_cmdJMP
+ * 13/07/2022:		Added mos_cmdSAVE, mos_cmdDEL, improved command parsing and file error reporting
  */
 
 #include <eZ80.h>
@@ -24,10 +26,40 @@ static t_mosCommand mosCommands[] = {
 	{ ".", 		&mos_cmdDIR },
 	{ "DIR",	&mos_cmdDIR },
 	{ "CAT",	&mos_cmdDIR },
-	{ "LOAD",	&mos_cmdLOAD }
+	{ "LOAD",	&mos_cmdLOAD },
+	{ "SAVE", 	&mos_cmdSAVE },
+	{ "DEL", 	&mos_cmdDEL },
+	{ "JMP",	&mos_cmdJMP }
 };
 
 #define mosCommands_count (sizeof(mosCommands)/sizeof(t_mosCommand))
+
+static char * mos_fileErrors[] = {
+	"OK",
+	"Error accessing SD card",
+	"Assertion failed",
+	"SD card failure",
+	"Could not find file",
+	"Could not find path",
+	"Invalid path name",
+	"Access denied due to prohibited access or directory full",
+	"Access denied due to prohibited access",
+	"Invalid file/directory object",
+	"SD card is write protected",
+	"Logical drive number is invalid",
+	"Volume has no work area",
+	"No valid FAT volume",
+	"Error occurred during mkfs",
+	"Volume timeout",
+	"Volume locked",
+	"LFN working buffer could not be allocated",
+	"Too many open files",
+	"Invalid parameter"
+};
+
+void mos_fileError(int error) {
+	printf("%c%s\n\r", MOS_prompt, mos_fileErrors[error]);
+}
 
 void mos_input(char * buffer, int bufferLength) {
 	INT ch = 0;
@@ -75,6 +107,39 @@ void * mos_getCommand(char * ptr) {
 	return 0;
 }
 
+BOOL mos_parseNumber(char * ptr, long * p_Value) {
+	char * 	p = ptr;
+	char * 	e;
+	int 	base = 10;
+	long 	value;
+
+	p = strtok(p, " ");
+	if(p == NULL) {
+		return 0;
+	}
+	if(*p == '&') {
+		base = 16;
+		p++;
+	}	
+	value = strtol(p, &e, base);
+	if(p == e) {
+		return 0;
+	}
+	*p_Value = value;
+	return 1;
+}
+
+BOOL mos_parseString(char * ptr, char ** p_Value) {
+	char *	p = ptr;
+
+	p = strtok(p, " ");
+	if(p == NULL) {
+		return 0;
+	}
+	*p_Value = p;
+	return 1;
+}
+
 void mos_exec(char * buffer, int bufferLength) {
 	char * 	ptr;
 	int 	status;
@@ -102,9 +167,20 @@ int mos_cmdDIR(char * ptr) {
 	int		yr, mo, da, hr, mi;
 	char 	str[12];
 	
-	f_getlabel("", str, 0);
+	fr = f_getlabel("", str, 0);
+	if(fr != 0) {
+		mos_fileError(fr);
+		return 0;
+	}
 	
-	printf("Volume: %s\n\r\n\r", str);
+	printf("Volume: ");
+	if(strlen(str) > 0) {
+		printf("%s", str);
+	}
+	else {
+		printf("<No Volume Label>");
+	}
+	printf("\n\r\n\r");
 	
 	fr = f_opendir(&dir, "/");
 	if(fr == FR_OK) {
@@ -118,16 +194,21 @@ int mos_cmdDIR(char * ptr) {
 			da = (fno.fdate & 0x001F);			// Bits  4 to  0
 			hr = (fno.ftime & 0xF800) >> 11;	// Bits 15 to 11
 			mi = (fno.ftime & 0x07E0) >>  5;	// Bits 10 to  5
-			printf("%04d/%02d/%02d\t%02d:%02d\t%s\n\r", yr + 1980, mo, da, hr, mi, fno.fname);
+			
+			printf("%04d/%02d/%02d\t%02d:%02d %c %*d %s\n\r", yr + 1980, mo, da, hr, mi, fno.fattrib & AM_DIR ? 'D' : ' ', 8, fno.fsize, fno.fname);
 		}
 	}
+	else {
+		mos_fileError(fr);	
+	}
 	f_closedir(&dir);
+	printf("\n\r");
 	return 0;
 }
 
 int mos_cmdLOAD(char * ptr) {
 	char *  filename;
-	char * 	addr;
+	long 	addr;
 	
 	FRESULT	fr;
 	FIL	   	fil;
@@ -135,30 +216,84 @@ int mos_cmdLOAD(char * ptr) {
 	void * 	dest;
 	FSIZE_t fSize;
 	
-	filename = strtok(NULL, " ");
-	addr = strtok(NULL, " ");
-	if(filename == NULL || addr == NULL) {
+	if(
+		!mos_parseString(NULL, &filename) ||
+		!mos_parseNumber(NULL, &addr)
+	) {
 		return 1;
 	}
-
-	dest = (void *)strtol(addr, NULL, 16);
+	
+	dest = (void *)addr;
 	
 	fr = f_open(&fil, filename, FA_READ);
 	if(fr == FR_OK) {
 		fSize = f_size(&fil);
 		fr = f_read(&fil, dest, fSize, &br);
-		if(fr == FR_OK) {
-			printf("%cLoaded file %s to address 0x%06X, %d bytes\n\r", MOS_prompt, filename, dest, fSize);
-		}
-		else {
-			printf("%cError loading file\n\r", MOS_prompt);
-		}
+		mos_fileError(fr);
 	}
 	else {
-		printf("%cFile not found\n\r", MOS_prompt);
+		mos_fileError(fr);
 		return 0;
 	}
 	f_close(&fil);	
+	return 0;
+}
 
+int mos_cmdSAVE(char * ptr) {
+	char *  filename;
+	long 	addr;
+	long 	size;
+	
+	FRESULT	fr;
+	FIL	   	fil;
+	UINT   	br;	
+	void * 	dest;
+	
+	if(
+		!mos_parseString(NULL, &filename) ||
+		!mos_parseNumber(NULL, &addr) ||
+		!mos_parseNumber(NULL, &size)
+	) {
+		return 1;
+	}
+	
+	dest = (void *)addr;
+	
+	fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_NEW);
+	if(fr == FR_OK) {
+		fr = f_write(&fil, dest, size, &br);
+		mos_fileError(fr);
+	}
+	else {
+		mos_fileError(fr);
+		return 0;
+	}
+	f_close(&fil);	
+	return 0;
+}
+
+int mos_cmdDEL(char * ptr) {
+	char *  filename;
+	
+	FRESULT	fr;
+	
+	if(
+		!mos_parseString(NULL, &filename) 
+	) {
+		return 1;
+	}
+	fr = f_unlink(filename);
+	mos_fileError(fr);
+	return 0;
+}
+
+int mos_cmdJMP(char *ptr) {
+	long 	addr;
+	void (* dest)(void) = 0;
+	if(!mos_parseNumber(NULL, &addr)) {
+		return 1;
+	};
+	dest = (void *)addr;
+	dest();
 	return 0;
 }
--- src/mos.h
+++ src/mos.h
@@ -2,10 +2,12 @@
  * Title:			AGON MOS - MOS code
  * Author:			Dean Belfield
  * Created:			10/07/2022
- * Last Updated:	11/07/2022
+ * Last Updated:	13/07/2022
  * 
  * Modinfo:
  * 11/07/2022:		Removed mos_cmdBYE, Added mos_cmdLOAD
+ * 12/07/2022:		Added mos_cmdJMP
+ * 13/07/2022:		Added mos_cmdSAVE
  */
 
 #ifndef MOS_H
@@ -16,11 +18,19 @@ typedef struct {
 	int (*func)(char * ptr);
 } t_mosCommand;
 
+void 	mos_fileError(int error);
+
 void	mos_input(char * buffer, int bufferLength);
 void *	mos_getCommand(char * ptr);
 void	mos_exec(char * buffer, int bufferLength);
 
+BOOL 	mos_parseNumber(char * ptr, long * p_Value);
+BOOL	mos_parseString(char * ptr, char ** p_Value);
+
 int		mos_cmdDIR(char * ptr);
 int		mos_cmdLOAD(char * ptr);
+int 	mos_cmdSAVE(char *ptr);
+int		mos_cmdDEL(char * ptr);
+int		mos_cmdJMP(char * ptr);
 
 #endif MOS_H
