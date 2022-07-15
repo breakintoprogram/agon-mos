909481de0c604ecd60503fad1198a7b7b6c570ed
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-07-15 18:52:37 +0100

    Added code for the VBLANK interrupt handler and GPIO

--- src_startup/init_params_f92.asm
+++ src_startup/init_params_f92.asm
@@ -3,167 +3,246 @@
 ; Author:	Copyright (C) 2005 by ZiLOG, Inc.  All Rights Reserved.
 ; Modified By:	Dean Belfield
 ; Created:	10/07/2022
-; Last Updated:	10/07/2022
+; Last Updated:	15/07/2022
 ;
 ; Modinfo:
-;
-		INCLUDE "ez80f92.inc"
+; 14/07/2022:	Added coldBoot detection, exec16 function
+; 15/07/2022:	Added further hardware/firmware initialisation in __init
 
+			INCLUDE	"../src/macros.inc"
+			INCLUDE	"../src/equs.inc"
 
-		XREF __stack
-		XREF __init_default_vectors
-		XREF __c_startup
-		XREF __cstartup
-		XREF _main
-		XREF __CS0_LBR_INIT_PARAM
-		XREF __CS0_UBR_INIT_PARAM
-		XREF __CS0_CTL_INIT_PARAM
-		XREF __CS1_LBR_INIT_PARAM
-		XREF __CS1_UBR_INIT_PARAM
-		XREF __CS1_CTL_INIT_PARAM
-		XREF __CS2_LBR_INIT_PARAM
-		XREF __CS2_UBR_INIT_PARAM
-		XREF __CS2_CTL_INIT_PARAM
-		XREF __CS3_LBR_INIT_PARAM
-		XREF __CS3_UBR_INIT_PARAM
-		XREF __CS3_CTL_INIT_PARAM
-		XREF __CS0_BMC_INIT_PARAM
-		XREF __CS1_BMC_INIT_PARAM
-		XREF __CS2_BMC_INIT_PARAM
-		XREF __CS3_BMC_INIT_PARAM
-		XREF __FLASH_CTL_INIT_PARAM
-		XREF __FLASH_ADDR_U_INIT_PARAM
-		XREF __RAM_CTL_INIT_PARAM
-		XREF __RAM_ADDR_U_INIT_PARAM
+			INCLUDE "ez80f92.inc"
 
-		XDEF __init
-		XDEF _abort
-		XDEF __exit
-		XDEF _exit
+			XREF	__stack
+			XREF	__init_default_vectors
+			XREF	__c_startup
+			XREF	__cstartup
+			XREF	_main
+			XREF	__CS0_LBR_INIT_PARAM
+			XREF	__CS0_UBR_INIT_PARAM
+			XREF	__CS0_CTL_INIT_PARAM
+			XREF	__CS1_LBR_INIT_PARAM
+			XREF	__CS1_UBR_INIT_PARAM
+			XREF	__CS1_CTL_INIT_PARAM
+			XREF	__CS2_LBR_INIT_PARAM
+			XREF	__CS2_UBR_INIT_PARAM
+			XREF	__CS2_CTL_INIT_PARAM
+			XREF	__CS3_LBR_INIT_PARAM
+			XREF	__CS3_UBR_INIT_PARAM
+			XREF	__CS3_CTL_INIT_PARAM
+			XREF	__CS0_BMC_INIT_PARAM
+			XREF	__CS1_BMC_INIT_PARAM
+			XREF	__CS2_BMC_INIT_PARAM
+			XREF	__CS3_BMC_INIT_PARAM
+			XREF	__FLASH_CTL_INIT_PARAM
+			XREF	__FLASH_ADDR_U_INIT_PARAM
+			XREF	__RAM_CTL_INIT_PARAM
+			XREF	__RAM_ADDR_U_INIT_PARAM
+
+			XDEF	__init
+			XDEF	_abort
+			XDEF	__exit
+			XDEF	_exit
+			
+			XDEF	__exec16
+			XDEF	_exec16
+			
+			XREF	GPIOB_SETMODE
 
 ; Startup code
 ;
-		DEFINE .STARTUP, SPACE = ROM
-		SEGMENT .STARTUP
-		.ASSUME ADL = 1
+			DEFINE .STARTUP, SPACE = ROM
+			SEGMENT .STARTUP
+			
+			.ASSUME ADL = 1
 
 ; Minimum default initialization
 ;
 ; Disable internal peripheral interrupt sources
 ; -- this will help during a RAM debug session --
 ;
-__init:		ld a, %FF
-		out0 (PB_DDR), a         ; GPIO
-		out0 (PC_DDR), a         ;
-		out0 (PD_DDR), a         ;
-		ld a, %00
-		out0 (PB_ALT1), a        ;
-		out0 (PC_ALT1), a        ;
-		out0 (PD_ALT1), a        ;
-		out0 (PB_ALT2), a        ;
-		out0 (PC_ALT2), a        ;
-		out0 (PD_ALT2), a        ;
-		out0 (TMR0_CTL), a       ; timers
-		out0 (TMR1_CTL), a       ;
-		out0 (TMR2_CTL), a       ;
-		out0 (TMR3_CTL), a       ;
-		out0 (TMR4_CTL), a       ;
-		out0 (TMR5_CTL), a       ;
-		out0 (UART0_IER), a      ; UARTs
-		out0 (UART1_IER), a      ;
-		out0 (I2C_CTL), a        ; I2C
-		out0 (FLASH_IRQ), a      ; Flash
-		ld a, %04
-		out0 (SPI_CTL), a        ; SPI
-		in0 a, (RTC_CTRL)        ; RTC, Writing to the RTC_CTRL register also
-		and a, %BE               ;      resets the RTC count prescaler allowing
-		out0 (RTC_CTRL), a       ;      the RTC to be synchronized to another time source
+__init:			ld a, %FF
+			out0 (PB_DDR), a         ; GPIO
+			out0 (PC_DDR), a         ;
+			out0 (PD_DDR), a         ;
+			ld a, %00
+			out0 (PB_ALT1), a        ;
+			out0 (PC_ALT1), a        ;
+			out0 (PD_ALT1), a        ;
+			out0 (PB_ALT2), a        ;
+			out0 (PC_ALT2), a        ;
+			out0 (PD_ALT2), a        ;
+			out0 (TMR0_CTL), a       ; timers
+			out0 (TMR1_CTL), a       ;
+			out0 (TMR2_CTL), a       ;
+			out0 (TMR3_CTL), a       ;
+			out0 (TMR4_CTL), a       ;
+			out0 (TMR5_CTL), a       ;
+			out0 (UART0_IER), a      ; UARTs
+			out0 (UART1_IER), a      ;
+			out0 (I2C_CTL), a        ; I2C
+			out0 (FLASH_IRQ), a      ; Flash
+			ld a, %04
+			out0 (SPI_CTL), a        ; SPI
+			in0 a, (RTC_CTRL)        ; RTC, Writing to the RTC_CTRL register also
+			and a, %BE               ;      resets the RTC count prescaler allowing
+			out0 (RTC_CTRL), a       ;      the RTC to be synchronized to another time source
 ;
 ; Configure external memory/io
 ;
-		ld a, __CS0_LBR_INIT_PARAM
-		out0 (CS0_LBR), a
-		ld a, __CS0_UBR_INIT_PARAM
-		out0 (CS0_UBR), a
-		ld a, __CS0_BMC_INIT_PARAM
-		out0 (CS0_BMC), a
-		ld a, __CS0_CTL_INIT_PARAM
-		out0 (CS0_CTL), a
+			ld a, __CS0_LBR_INIT_PARAM
+			out0 (CS0_LBR), a
+			ld a, __CS0_UBR_INIT_PARAM
+			out0 (CS0_UBR), a
+			ld a, __CS0_BMC_INIT_PARAM
+			out0 (CS0_BMC), a
+			ld a, __CS0_CTL_INIT_PARAM
+			out0 (CS0_CTL), a
 
-		ld a, __CS1_LBR_INIT_PARAM
-		out0 (CS1_LBR), a
-		ld a, __CS1_UBR_INIT_PARAM
-		out0 (CS1_UBR), a
-		ld a, __CS1_BMC_INIT_PARAM
-		out0 (CS1_BMC), a
-		ld a, __CS1_CTL_INIT_PARAM
-		out0 (CS1_CTL), a
+			ld a, __CS1_LBR_INIT_PARAM
+			out0 (CS1_LBR), a
+			ld a, __CS1_UBR_INIT_PARAM
+			out0 (CS1_UBR), a
+			ld a, __CS1_BMC_INIT_PARAM
+			out0 (CS1_BMC), a
+			ld a, __CS1_CTL_INIT_PARAM
+			out0 (CS1_CTL), a
 
-		ld a, __CS2_LBR_INIT_PARAM
-		out0 (CS2_LBR), a
-		ld a, __CS2_UBR_INIT_PARAM
-		out0 (CS2_UBR), a
-		ld a, __CS2_BMC_INIT_PARAM
-		out0 (CS2_BMC), a
-		ld a, __CS2_CTL_INIT_PARAM
-		out0 (CS2_CTL), a
+			ld a, __CS2_LBR_INIT_PARAM
+			out0 (CS2_LBR), a
+			ld a, __CS2_UBR_INIT_PARAM
+			out0 (CS2_UBR), a
+			ld a, __CS2_BMC_INIT_PARAM
+			out0 (CS2_BMC), a
+			ld a, __CS2_CTL_INIT_PARAM
+			out0 (CS2_CTL), a
 
-		ld a, __CS3_LBR_INIT_PARAM
-		out0 (CS3_LBR), a
-		ld a, __CS3_UBR_INIT_PARAM
-		out0 (CS3_UBR), a
-		ld a, __CS3_BMC_INIT_PARAM
-		out0 (CS3_BMC), a
-		ld a, __CS3_CTL_INIT_PARAM
-		out0 (CS3_CTL), a
+			ld a, __CS3_LBR_INIT_PARAM
+			out0 (CS3_LBR), a
+			ld a, __CS3_UBR_INIT_PARAM
+			out0 (CS3_UBR), a
+			ld a, __CS3_BMC_INIT_PARAM
+			out0 (CS3_BMC), a
+			ld a, __CS3_CTL_INIT_PARAM
+			out0 (CS3_CTL), a
 ;   
 ; Enable internal memory
 ;
-		ld a, __FLASH_ADDR_U_INIT_PARAM
-		out0 (FLASH_ADDR_U), a
-		ld a, __FLASH_CTL_INIT_PARAM
-		out0 (FLASH_CTRL), a
+			ld a, __FLASH_ADDR_U_INIT_PARAM
+			out0 (FLASH_ADDR_U), a
+			ld a, __FLASH_CTL_INIT_PARAM
+			out0 (FLASH_CTRL), a
 
-		ld a, __RAM_ADDR_U_INIT_PARAM
-		out0 (RAM_ADDR_U), a
-		ld a, __RAM_CTL_INIT_PARAM
-		out0 (RAM_CTL), a
+			ld a, __RAM_ADDR_U_INIT_PARAM
+			out0 (RAM_ADDR_U), a
+			ld a, __RAM_CTL_INIT_PARAM
+			out0 (RAM_CTL), a
 ;
 ; Setup Stack Pointer
 ;
-		ld sp, __stack
+			ld sp, __stack
+;
+; Detect warm or cold boot
+;
+			ld a, i			; Register I should be 0 on cold boot
+			or a, a		
+			ld a, 1			; Set to 1 if cold boot
+			jr z, __coldBoot
+			dec a			; Otherwise set to 0
+__coldBoot:		push af 		; Stack AF, will store later as __c_startup clears globals area
 ;
 ; Initialize the interrupt vector table
 ;
-		call __init_default_vectors
+			call __init_default_vectors
+;
+; Further hardware/firmware initialisation
+;
+			LD	A, GPIOMODE_INTRE
+			LD	B, 2
+			CALL	GPIOB_SETMODE
 ;
 ; Start application
 ;
-		ld a, __cstartup
-		or a, a
-		jr z, __no_cstartup
-		call __c_startup
+			ld a, __cstartup
+			or a, a
+			jr z, __no_cstartup
+			call __c_startup
+
+			pop af			; Pop the coldboot value
+			ld (_coldBoot), a	; And store
 
 
-
-__no_cstartup:	ld hl, 0                ; hl = NULL
-		push hl                 ; argv[0] = NULL
-		ld ix, 0
-		add ix, sp              ; ix = &argv[0]
-		push ix                 ; &argv[0]
-		pop hl
-		ld de, 0                ; argc = 0
-		call _main              ; int main(int argc, char *argv[]))
-		pop de			; clean the stack
+__no_cstartup:		ld hl, 0                ; hl = NULL
+			push hl                 ; argv[0] = NULL
+			ld ix, 0
+			add ix, sp              ; ix = &argv[0]
+			push ix                 ; &argv[0]
+			pop hl
+			ld de, 0                ; argc = 0
+			call _main              ; int main(int argc, char *argv[]))
+			pop de			; clean the stack
 
 __exit:
 _exit:
-_abort:		jr $                	; If we return from main loop forever here
+_abort:			jr $                	; If we return from main loop forever here
 
-    
-_SysClkFreq:	DL _SYS_CLK_FREQ
+; Execute a program in RAM
+; void * _exec16(unsigned long address)
+; Params:
+; - address: The 24-bit address to call
+;
+; This function will call the 24 bit address and switch the eZ80 into Z80 mode (ADL=0)
+; The called function must do a RET.LIS (49h, C9h) and take care of preserving registers
+;
+__exec16:
+_exec16:		PUSH 	IY
+			LD	IY, 0
+			ADD	IY, SP		; Standard prologue
+			PUSH 	AF		; Stack any registers being used
+			PUSH	DE
+			PUSH	HL
+			LD	A, MB
+			PUSH	AF
+			LD	DE, (IY+6)	; Get the address
+			LD	A, (IY+8)	; And the high byte for the code segment
+			LD	MB, A		; Set the MBASE register		
+;
+; Write out a short subroutine "CALL.IS (DE): RET" to RAM
+;
+			LD	IY,_callSM	; Storage for the self modified routine
+			LD	(IY + 0), 49h	; CALL.IS llhh
+			LD	(IY + 1), CDh
+			LD	(IY + 2), E
+			LD	(IY + 3), D
+			LD	(IY + 4), C9h	; RET		
+			CALL	_callSM		; Call the subroutine
+			
+			POP	AF		; Restore the MBASE register
+			LD	MB, A
+			POP	HL		; Balance the stack
+			POP	DE
+			POP 	AF
+			LD	SP, IY          ; Standard epilogue
+			POP	IY
+			RET		 
 
-		XREF _SYS_CLK_FREQ
-		XDEF _SysClkFreq
+; Define global constants
+;
+			XREF _SYS_CLK_FREQ
+			XDEF _SysClkFreq
 
-		END
+_SysClkFreq:		DL _SYS_CLK_FREQ
+	
+; Define global vars
+;
+			SEGMENT DATA
+		
+			XDEF _coldBoot		
+			XDEF _keycode
+
+_coldBoot:		DS 1			; extern char _coldBoot
+_keycode:		DS 1
+_callSM:		DS 5			; Self-modding code for CALL.IS (HL)
+	
+			END
--- src_startup/vectors16.asm
+++ src_startup/vectors16.asm
@@ -3,98 +3,125 @@
 ; Author:	Copyright (C) 2005 by ZiLOG, Inc.  All Rights Reserved.
 ; Modified By:	Dean Belfield
 ; Created:	10/07/2022
-; Last Updated:	11/07/2022
+; Last Updated:	15/07/2022
 ;
 ; Modinfo:
 ; 11/07/2022:	Added RST_10 code - TX
+; 15/07/2022:	Added __vertical_blank_handler
 
-		XREF __init
-		XREF __low_rom
-		
-		XDEF _reset
-		XDEF __default_nmi_handler
-		XDEF __default_mi_handler
-		XDEF __nvectors
-		XDEF _init_default_vectors
-		XDEF __init_default_vectors
-		XDEF _set_vector
-		XDEF __set_vector
-		XDEF __2nd_jump_table
-		XDEF __1st_jump_table
-		XDEF __vector_table
+			INCLUDE	"../src/macros.inc"
+			INCLUDE	"../src/equs.inc"
 
-		XREF serial_TX
+			XREF 	__init
+			XREF 	__low_rom
+			
+			XDEF 	_reset
+			XDEF 	__default_nmi_handler
+			XDEF 	__default_mi_handler
+			XDEF 	__nvectors
+			XDEF 	_init_default_vectors
+			XDEF 	__init_default_vectors
+			XDEF 	_set_vector
+			XDEF	__set_vector
+			XDEF	__2nd_jump_table
+			XDEF	__1st_jump_table
+			XDEF	__vector_table
 
-NVECTORS 	EQU 48			; Number of interrupt vectors
+			XDEF	_vblank_handler
+			
+			XREF	_keycode
+			XREF	serial_TX
+			XREF	serial_RX
+
+NVECTORS 		EQU 48			; Number of interrupt vectors
 
 ; Save Interrupt State
 ;
-SAVEIMASK	MACRO
-		LD	A, I		; Sets parity bit to value of IEF2
-		PUSH	AF
-		DI			; Disable interrupts while loading table 
-		MACEND
+SAVEIMASK		MACRO
+			LD	A, I		; Sets parity bit to value of IEF2
+			PUSH	AF
+			DI			; Disable interrupts while loading table 
+			MACEND
 
 ; Restore Interrupt State
 ;
-RESTOREIMASK	MACRO
-		POP	AF
-		JP	PO, $+5		; Parity bit is IEF2
-		EI
-		MACEND
+RESTOREIMASK		MACRO
+			POP	AF
+			JP	PO, $+5		; Parity bit is IEF2
+			EI
+			MACEND
 
 ; Reset and all RST nn's
 ;
-		DEFINE .RESET, SPACE = ROM
-		SEGMENT .RESET
+			DEFINE .RESET, SPACE = ROM
+			SEGMENT .RESET
 
 _reset:	
-_rst0:		DI
-		RSMIX
-		JP.LIL	__init
+_rst0:			DI
+			RSMIX
+			JP.LIL	__init
 		
-_rst8:		RET
-		DS	7
+_rst8:			RET
+			DS	7
 		
-_rst10:		JP	serial_TX
-		DS	5
+_rst10:			JP.LIL	serial_TX
+			DS	3
 
-_rst18:		RET
-		DS	7
+_rst18:			RET
+			DS	7
 		
-_rst20:		RET
-		DS	7
+_rst20:			RET
+			DS	7
 		
-_rst28:		RET
-		DS	7
+_rst28:			RET
+			DS	7
 
-_rst30:		RET
-		DS	7
+_rst30:			RET
+			DS	7
 
-_rst38:		RET
-		DS 	%2D
+_rst38:			RET
+			DS 	%2D
 		
-_nmi:		JP.LIL	__default_nmi_handler
+_nmi:			JP.LIL	__default_nmi_handler
 
+;
 ; Startup code
-		DEFINE .STARTUP, SPACE = ROM
-		SEGMENT .STARTUP
-		.ASSUME ADL=1
+;
+			DEFINE .STARTUP, SPACE = ROM
+			SEGMENT .STARTUP
+
+			.ASSUME ADL=1
 
 ; Number of vectors supported
 ;
-__nvectors:
-		DW NVECTORS            ; extern unsigned short _num_vectors;
+__nvectors:		DW NVECTORS            ; extern unsigned short _num_vectors;
 
 ; Default Non-Maskable Interrupt handler
 ;
-__default_nmi_handler:
-		RETN
+__default_nmi_handler:	RETN
 
 ; Default Maskable Interrupt handler
-__default_mi_handler:
-		EI
-		RETI
+;
+__default_mi_handler:	EI
+			RETI
+		
+; AGON Vertical Blank Interrupt handler
+;
+_vblank_handler:	DI
+			PUSH		AF
+			SET_GPIO 	PB_DR, 2	; Need to set this to 1 for the interrupt to work correctly
+			PUSH		BC
+			PUSH		DE
+			PUSH		HL
+			CALL		serial_RX
+			JR		NC, _vblank_handler1
+			LD		(_keycode), A
+_vblank_handler1:	POP		HL
+			POP		DE
+			POP		BC
+			POP		AF
+			EI	
+			RETI
 
 ; Initialize all potential interrupt vector locations with a known
 ; default handler.
@@ -102,64 +129,60 @@ __default_mi_handler:
 ; void _init_default_vectors(void);
 ;
 __init_default_vectors:
-_init_default_vectors:
-		push af
-		SAVEIMASK
-		ld hl, __default_mi_handler
-		ld a, %C3
-		ld (__2nd_jump_table), a       ; place jp opcode
-		ld (__2nd_jump_table + 1), hl  ; __default_hndlr
-		ld hl, __2nd_jump_table
-		ld de, __2nd_jump_table + 4
-		ld bc, NVECTORS * 4 - 4
-		ldir
-		im 2                       ; Interrtup mode 2
-		ld a, __vector_table >> 8
-		ld i, a                    ; Load interrtup vector base
-		RESTOREIMASK
-		pop af
-		ret
+_init_default_vectors:	push af
+			SAVEIMASK
+			ld hl, __default_mi_handler
+			ld a, %C3
+			ld (__2nd_jump_table), a       ; place jp opcode
+			ld (__2nd_jump_table + 1), hl  ; __default_hndlr
+			ld hl, __2nd_jump_table
+			ld de, __2nd_jump_table + 4
+			ld bc, NVECTORS * 4 - 4
+			ldir
+			im 2                       ; Interrupt mode 2
+			ld a, __vector_table >> 8
+			ld i, a                    ; Load interrupt vector base
+			RESTOREIMASK
+			pop af
+			ret
 
 ; Installs a user interrupt handler in the 2nd interrupt vector jump table
 ;
 ; void * _set_vector(unsigned int vector, void(*handler)(void));
 ;
 __set_vector:
-_set_vector:	
-		push iy
-		ld iy, 0
-		add iy, sp                 ; Standard prologue
-		push af
-		SAVEIMASK
-		ld bc, 0                   ; clear bc
-		ld b, 2                    ; calculate 2nd jump table offset
-		ld c, (iy+6)               ; vector offset
-		mlt bc                     ; bc is 2nd jp table offset
-		ld hl, __2nd_jump_table
-		add hl, bc                 ; hl is location of jp in 2nd jp table
-		ld (hl), %C3               ; place jp opcode just in case
-		inc hl                     ; hl is jp destination address
-		ld bc, (iy+9)              ; bc is isr address
-		ld de, (hl)                ; save previous handler
-		ld (hl), bc                ; store new isr address
-		push de
-		pop hl                     ; return previous handler
-		RESTOREIMASK
-		pop af
-		ld sp, iy                  ; standard epilogue
-		pop iy
-		ret
+_set_vector:		push iy
+			ld iy, 0
+			add iy, sp                 ; Standard prologue
+			push af
+			SAVEIMASK
+			ld bc, 0                   ; clear bc
+			ld b, 2                    ; calculate 2nd jump table offset
+			ld c, (iy+6)               ; vector offset
+			mlt bc                     ; bc is 2nd jp table offset
+			ld hl, __2nd_jump_table
+			add hl, bc                 ; hl is location of jp in 2nd jp table
+			ld (hl), %C3               ; place jp opcode just in case
+			inc hl                     ; hl is jp destination address
+			ld bc, (iy+9)              ; bc is isr address
+			ld de, (hl)                ; save previous handler
+			ld (hl), bc                ; store new isr address
+			push de
+			pop hl                     ; return previous handler
+			RESTOREIMASK
+			pop af
+			ld sp, iy                  ; standard epilogue
+			pop iy
+			ret
 
-
-		DEFINE IVJMPTBL, SPACE = RAM
-		SEGMENT IVJMPTBL
+			DEFINE IVJMPTBL, SPACE = RAM
+			SEGMENT IVJMPTBL
 
 ; 2nd Interrupt Vector Jump Table
 ;  - this table must reside in RAM anywhere in the 16M byte range
 ;  - each 4-byte entry is a jump to an interrupt handler
 ;
-__2nd_jump_table:
-		DS NVECTORS * 4
+__2nd_jump_table:	DS NVECTORS * 4
 
 
 ; Interrupt Vector Table
@@ -167,111 +190,109 @@ __2nd_jump_table:
 ;    the 64K byte boundry
 ;  - each 2-byte entry is a 2-byte vector address
 ;
-		DEFINE .IVECTS, SPACE = ROM, ALIGN = 100h
-		SEGMENT .IVECTS
+			DEFINE .IVECTS, SPACE = ROM, ALIGN = 100h
+			SEGMENT .IVECTS
 
-__vector_table:	
-		dw __1st_jump_table + %00
-		dw __1st_jump_table + %04
-		dw __1st_jump_table + %08
-		dw __1st_jump_table + %0c
-		dw __1st_jump_table + %10
-		dw __1st_jump_table + %14
-		dw __1st_jump_table + %18
-		dw __1st_jump_table + %1c
-		dw __1st_jump_table + %20
-		dw __1st_jump_table + %24
-		dw __1st_jump_table + %28
-		dw __1st_jump_table + %2c
-		dw __1st_jump_table + %30
-		dw __1st_jump_table + %34
-		dw __1st_jump_table + %38
-		dw __1st_jump_table + %3c
-		dw __1st_jump_table + %40
-		dw __1st_jump_table + %44
-		dw __1st_jump_table + %48
-		dw __1st_jump_table + %4c
-		dw __1st_jump_table + %50
-		dw __1st_jump_table + %54
-		dw __1st_jump_table + %58
-		dw __1st_jump_table + %5c
-		dw __1st_jump_table + %60
-		dw __1st_jump_table + %64
-		dw __1st_jump_table + %68
-		dw __1st_jump_table + %6c
-		dw __1st_jump_table + %70
-		dw __1st_jump_table + %74
-		dw __1st_jump_table + %78
-		dw __1st_jump_table + %7c
-		dw __1st_jump_table + %80
-		dw __1st_jump_table + %84
-		dw __1st_jump_table + %88
-		dw __1st_jump_table + %8c
-		dw __1st_jump_table + %90
-		dw __1st_jump_table + %94
-		dw __1st_jump_table + %98
-		dw __1st_jump_table + %9c
-		dw __1st_jump_table + %a0
-		dw __1st_jump_table + %a4
-		dw __1st_jump_table + %a8
-		dw __1st_jump_table + %ac
-		dw __1st_jump_table + %b0
-		dw __1st_jump_table + %b4
-		dw __1st_jump_table + %b8
-		dw __1st_jump_table + %bc
+__vector_table:		dw __1st_jump_table + %00
+			dw __1st_jump_table + %04
+			dw __1st_jump_table + %08
+			dw __1st_jump_table + %0c
+			dw __1st_jump_table + %10
+			dw __1st_jump_table + %14
+			dw __1st_jump_table + %18
+			dw __1st_jump_table + %1c
+			dw __1st_jump_table + %20
+			dw __1st_jump_table + %24
+			dw __1st_jump_table + %28
+			dw __1st_jump_table + %2c
+			dw __1st_jump_table + %30
+			dw __1st_jump_table + %34
+			dw __1st_jump_table + %38
+			dw __1st_jump_table + %3c
+			dw __1st_jump_table + %40
+			dw __1st_jump_table + %44
+			dw __1st_jump_table + %48
+			dw __1st_jump_table + %4c
+			dw __1st_jump_table + %50
+			dw __1st_jump_table + %54
+			dw __1st_jump_table + %58
+			dw __1st_jump_table + %5c
+			dw __1st_jump_table + %60
+			dw __1st_jump_table + %64
+			dw __1st_jump_table + %68
+			dw __1st_jump_table + %6c
+			dw __1st_jump_table + %70
+			dw __1st_jump_table + %74
+			dw __1st_jump_table + %78
+			dw __1st_jump_table + %7c
+			dw __1st_jump_table + %80
+			dw __1st_jump_table + %84
+			dw __1st_jump_table + %88
+			dw __1st_jump_table + %8c
+			dw __1st_jump_table + %90
+			dw __1st_jump_table + %94
+			dw __1st_jump_table + %98
+			dw __1st_jump_table + %9c
+			dw __1st_jump_table + %a0
+			dw __1st_jump_table + %a4
+			dw __1st_jump_table + %a8
+			dw __1st_jump_table + %ac
+			dw __1st_jump_table + %b0
+			dw __1st_jump_table + %b4
+			dw __1st_jump_table + %b8
+			dw __1st_jump_table + %bc
 
 ; 1st Interrupt Vector Jump Table
 ;  - this table must reside in the first 64K bytes of memory
 ;  - each 4-byte entry is a jump to the 2nd jump table plus offset
 ;
-__1st_jump_table:
-		jp __2nd_jump_table + %00
-		jp __2nd_jump_table + %04
-		jp __2nd_jump_table + %08
-		jp __2nd_jump_table + %0c
-		jp __2nd_jump_table + %10
-		jp __2nd_jump_table + %14
-		jp __2nd_jump_table + %18
-		jp __2nd_jump_table + %1c
-		jp __2nd_jump_table + %20
-		jp __2nd_jump_table + %24
-		jp __2nd_jump_table + %28
-		jp __2nd_jump_table + %2c
-		jp __2nd_jump_table + %30
-		jp __2nd_jump_table + %34
-		jp __2nd_jump_table + %38
-		jp __2nd_jump_table + %3c
-		jp __2nd_jump_table + %40
-		jp __2nd_jump_table + %44
-		jp __2nd_jump_table + %48
-		jp __2nd_jump_table + %4c
-		jp __2nd_jump_table + %50
-		jp __2nd_jump_table + %54
-		jp __2nd_jump_table + %58
-		jp __2nd_jump_table + %5c
-		jp __2nd_jump_table + %60
-		jp __2nd_jump_table + %64
-		jp __2nd_jump_table + %68
-		jp __2nd_jump_table + %6c
-		jp __2nd_jump_table + %70
-		jp __2nd_jump_table + %74
-		jp __2nd_jump_table + %78
-		jp __2nd_jump_table + %7c
-		jp __2nd_jump_table + %80
-		jp __2nd_jump_table + %84
-		jp __2nd_jump_table + %88
-		jp __2nd_jump_table + %8c
-		jp __2nd_jump_table + %90
-		jp __2nd_jump_table + %94
-		jp __2nd_jump_table + %98
-		jp __2nd_jump_table + %9c
-		jp __2nd_jump_table + %a0
-		jp __2nd_jump_table + %a4
-		jp __2nd_jump_table + %a8
-		jp __2nd_jump_table + %ac
-		jp __2nd_jump_table + %b0
-		jp __2nd_jump_table + %b4
-		jp __2nd_jump_table + %b8
-		jp __2nd_jump_table + %bc
+__1st_jump_table:	jp __2nd_jump_table + %00
+			jp __2nd_jump_table + %04
+			jp __2nd_jump_table + %08
+			jp __2nd_jump_table + %0c
+			jp __2nd_jump_table + %10
+			jp __2nd_jump_table + %14
+			jp __2nd_jump_table + %18
+			jp __2nd_jump_table + %1c
+			jp __2nd_jump_table + %20
+			jp __2nd_jump_table + %24
+			jp __2nd_jump_table + %28
+			jp __2nd_jump_table + %2c
+			jp __2nd_jump_table + %30
+			jp __2nd_jump_table + %34
+			jp __2nd_jump_table + %38
+			jp __2nd_jump_table + %3c
+			jp __2nd_jump_table + %40
+			jp __2nd_jump_table + %44
+			jp __2nd_jump_table + %48
+			jp __2nd_jump_table + %4c
+			jp __2nd_jump_table + %50
+			jp __2nd_jump_table + %54
+			jp __2nd_jump_table + %58
+			jp __2nd_jump_table + %5c
+			jp __2nd_jump_table + %60
+			jp __2nd_jump_table + %64
+			jp __2nd_jump_table + %68
+			jp __2nd_jump_table + %6c
+			jp __2nd_jump_table + %70
+			jp __2nd_jump_table + %74
+			jp __2nd_jump_table + %78
+			jp __2nd_jump_table + %7c
+			jp __2nd_jump_table + %80
+			jp __2nd_jump_table + %84
+			jp __2nd_jump_table + %88
+			jp __2nd_jump_table + %8c
+			jp __2nd_jump_table + %90
+			jp __2nd_jump_table + %94
+			jp __2nd_jump_table + %98
+			jp __2nd_jump_table + %9c
+			jp __2nd_jump_table + %a0
+			jp __2nd_jump_table + %a4
+			jp __2nd_jump_table + %a8
+			jp __2nd_jump_table + %ac
+			jp __2nd_jump_table + %b0
+			jp __2nd_jump_table + %b4
+			jp __2nd_jump_table + %b8
+			jp __2nd_jump_table + %bc
 
-		END
+			END
