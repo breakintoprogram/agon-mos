df65a529877d7f585fda1eeeeb538bdd99d2cdb8
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-07-25 23:13:12 +0100

    Moved interrupt handler to vectors16.asm and initialisation to main

--- src/timer.c
+++ src/timer.c
@@ -3,39 +3,28 @@
  * Author:			Cocoacrumbs
  * Modified by:		Dean Belfield
  * Created:			19/06/2022
- * Last Updated:	19/06/2022
+ * Last Updated:	24/07/2022
  * 
  * Thank you to @CoCoaCrumbs fo this code https://www.cocoacrumbs.com/
  *
  * Modinfo:
  * 11/07/2022:		Removed unused functions
+ * 24/07/2022:		Moved interrupt handler to vectors16.asm and initialisation to main
  */
 
 #include <eZ80.h>
-
-#define TMR2_IVECT 0x0e
-
-static unsigned int volatile g_timer = 0;
+#include <defines.h>
 
 extern long SysClkFreq;
-
-void * set_vector(unsigned int vector, void (*hndlr)(void));
-
-void interrupt timer2_isr(void) {
-	unsigned char tmp;
-	tmp = TMR2_CTL;
-    g_timer++;
-} 
+extern volatile	UINT24 timer2;
 
 void init_timer2(int interval) {
 	unsigned char tmp;
 	unsigned short rr;
+	
+	timer2 = 0;
 
 	TMR2_CTL = 0x00;
-
-	// Set Timer 2 interrupt vector 
-	//
-    set_vector(TMR2_IVECT, timer2_isr);
 
 	rr = (unsigned short)(((SysClkFreq / 1000) * interval) / 16);
 
@@ -47,7 +36,7 @@ void init_timer2(int interval) {
 }
 
 void delayms(int ms) {  
-    g_timer = 0;
-	while (g_timer < ms); 
+    timer2 = 0;
+	while (timer2  < ms); 
 } 
 
