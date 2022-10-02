e5f7a46102cf90531149309cf6b013cb570e7011
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-10-02 15:44:23 +0100

    Added mos_GETERROR, mos_MKDIR

--- src/mos.h
+++ src/mos.h
@@ -2,7 +2,7 @@
  * Title:			AGON MOS - MOS code
  * Author:			Dean Belfield
  * Created:			10/07/2022
- * Last Updated:	05/09/2022
+ * Last Updated:	25/09/2022
  * 
  * Modinfo:
  * 11/07/2022:		Removed mos_cmdBYE, Added mos_cmdLOAD
@@ -13,6 +13,7 @@
  * 03/08/2022:		Added a handful of MOS API calls
  * 05/08/2022:		Added mos_FEOF
  * 05/09/2022:		Added mos_cmdREN, mos_cmdBOOT; moved mos_EDITLINE into mos_editline.c
+ * 25/09/2022:		Added mos_GETERROR, mos_MKDIR
  */
 
 #ifndef MOS_H
@@ -48,6 +49,7 @@ int		mos_cmdJMP(char * ptr);
 int		mos_cmdRUN(char * ptr);
 int		mos_cmdCD(char * ptr);
 int		mos_cmdREN(char *ptr);
+int		mos_cmdMKDIR(char *ptr);
 
 UINT24	mos_LOAD(char * filename, INT24 address, INT24 size);
 UINT24	mos_SAVE(char * filename, INT24 address, INT24 size);
@@ -55,6 +57,7 @@ UINT24	mos_CD(char * path);
 UINT24	mos_DIR(void);
 UINT24	mos_DEL(char * filename);
 UINT24 	mos_REN(char * filename1, char * filename2);
+UINT24	mos_MKDIR(char * filename);
 UINT24 	mos_BOOT(char * filename, char * buffer, INT24 size);
 
 UINT24	mos_FOPEN(char * filename, UINT8 mode);
@@ -63,5 +66,7 @@ char	mos_FGETC(UINT8 fh);
 void	mos_FPUTC(UINT8 fh, char c);
 
 char	mos_FEOF(UINT8 fh);
+
+void 	mos_GETERROR(UINT8 errno, INT24 address, INT24 size);
 
 #endif MOS_H
