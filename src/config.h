e0a6b9491a9060f3d39ac98ad2f72ab742c935f1
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-11-13 22:32:35 +0000

    Add files via upload

--- src/config.h
+++ src/config.h
@@ -2,9 +2,10 @@
  * Title:			AGON MOS - MOS config
  * Author:			Dean Belfield
  * Created:			19/09/2022
- * Last Updated:	19/09/2022
+ * Last Updated:	13/11/2022
  * 
  * Modinfo:
+ * 13/11/2022:		Added MOS_starLoadAddress
  */
 
 #ifndef CONFIG_H
@@ -15,5 +16,6 @@
 #define MOS_prompt '*'						// MOS prompt character
 #define MOS_maxOpenFiles 8					// Maximum number of files that mos_FOPEN can open at the same time
 #define MOS_defaultLoadAddress	0x040000	// Default load address for LOAD and RUN commands
+#define MOS_starLoadAddress 0xB0000			// Address for loading on-SD star commands
 
 #endif CONFIG_H
