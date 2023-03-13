5b75d24e1812ba78520dcef9b5b8f5aa643c5c63
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-13 19:16:47 +0000

    Refactored

--- src/timer.c
+++ src/timer.c
@@ -1,42 +1,69 @@
 /*
  * Title:			AGON MOS - Timer
- * Author:			Cocoacrumbs
- * Modified by:		Dean Belfield
+ * Author:			Dean Belfield
  * Created:			19/06/2022
- * Last Updated:	24/07/2022
+ * Last Updated:	08/09/2023
  * 
- * Thank you to @CoCoaCrumbs fo this code https://www.cocoacrumbs.com/
- *
  * Modinfo:
  * 11/07/2022:		Removed unused functions
  * 24/07/2022:		Moved interrupt handler to vectors16.asm and initialisation to main
+ * 08/09/2023:		Refactored
  */
 
 #include <eZ80.h>
 #include <defines.h>
 
 extern long SysClkFreq;
-extern volatile	UINT24 timer2;
 
-void init_timer2(int interval) {
-	unsigned char tmp;
-	unsigned short rr;
-	
-	timer2 = 0;
+// Configure Timer 0
+// Parameters:
+// - interval: Interval in ms
+// - clkdiv: 4, 16, 64 or 256
+// - clkflag: Other clock flags (interrupt, etc)
+// Returns:
+// - interval value
+//
+unsigned short init_timer0(int interval, int clkdiv, unsigned char ctrlbits) {
+	unsigned short	rr;
+	unsigned char	clkbits = 0;
+	unsigned char	ctl;
 
-	TMR2_CTL = 0x00;
+	switch(clkdiv) {
+		case  16: clkbits = 0x04; break;
+		case  64: clkbits = 0x08; break;	
+		case 256: clkbits = 0x0C; break;
+	}
+	ctl = (ctrlbits | clkbits);
 
-	rr = (unsigned short)(((SysClkFreq / 1000) * interval) / 16);
+	rr = (unsigned short)((SysClkFreq / 100) / clkdiv) * interval;
 
-	TMR2_RR_H = (unsigned char)(rr >> 8);
-	TMR2_RR_L = (unsigned char)(rr);
+	TMR0_CTL = 0x00;													// Disable the timer and clear all settings	
+	TMR0_RR_L = (unsigned char)(rr);
+	TMR0_RR_H = (unsigned char)(rr >> 8);
+    TMR0_CTL = ctl;
 
-	tmp = TMR2_CTL;
-    TMR2_CTL = 0x57;
+	return rr;
 }
 
-void delayms(int ms) {  
-    timer2 = 0;
-	while (timer2  < ms); 
-} 
+// Enable Timer 0
+// Parameters:
+// - enable: 0 = disable, 1 = enable
+//
+void enable_timer0(unsigned char enable) {
+	unsigned char b;
 
+	if(enable <= 1) {
+		b = TMR0_CTL;
+		b &= 0xFC;
+		b |= (enable | 2); 
+		TMR0_CTL = b;	
+	}
+}
+
+// Get data count of Timer 0
+//
+unsigned short get_timer0() {
+	unsigned char l = TMR0_DR_L;
+	unsigned char h = TMR0_DR_H;
+	return (h << 8) | l;
+}
--- src/timer.h
+++ src/timer.h
@@ -3,18 +3,20 @@
  * Author:			Cocoacrumbs
  * Modified by:		Dean Belfield
  * Created:			19/06/2022
- * Last Updated:	19/06/2022
+ * Last Updated:	09/03/2023
  * 
- * Thank you to @CoCoaCrumbs fo this code https://www.cocoacrumbs.com/
- *
  * Modinfo:
  * 11/07/2022:		Removed unused functions
+ * 09/03/2023:      Refactored
  */
 
 #ifndef TIMER_H
 #define TIMER_H
 
-void init_timer2(int interval);
-void delayms(int ms);
+unsigned short  init_timer0(int interval, int clkdiv, unsigned char ctrlbits);
+void            enable_timer0(unsigned char enable);
+unsigned short  get_timer0();
+
+void            wait_timer0();  // In misc.asm
 
 #endif TIMER_H
