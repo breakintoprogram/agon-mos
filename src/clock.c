d86ccaae59244e179b81c7dd1bfe179091cc2f52
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-13 19:21:03 +0000

    Add files via upload

---
+++ src/clock.c
@@ -0,0 +1,14 @@
+/*
+ * Title:			AGON MOS - Real Time Clock
+ * Author:			Dean Belfield
+ * Created:			09/03/2023
+ * Last Updated:	09/03/2023
+ * 
+ * Modinfo:
+ */
+
+#include <ez80.h>
+#include <defines.h>
+
+#include "clock.h"
+
---
+++ src/clock.h
@@ -0,0 +1,15 @@
+/*
+ * Title:			AGON MOS - Real Time Clock
+ * Author:			Dean Belfield
+ * Created:			09/03/2023
+ * Last Updated:	09/03/2023
+ *
+ * Modinfo:
+ */
+
+#ifndef RTC_H
+#define RTC_H
+
+void init_rtc();            // In rtc.asm
+
+#endif RTC_H
