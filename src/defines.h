7217e26414c3e00656d0290c2aab4a773fd853b2
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-22 22:45:25 +0000

    The VDP commands are now indexed from 0x80

--- src/defines.h
+++ src/defines.h
@@ -2,9 +2,10 @@
  * Title:			AGON MOS - MOS defines
  * Author:			Dean Belfield
  * Created:			21/03/2023
- * Last Updated:	21/03/2023
+ * Last Updated:	22/03/2023
  * 
  * Modinfo:
+ * 22/03/2023:		The VDP commands are now indexed from 0x80
  */
 
 #ifndef MOS_DEFINES_H
@@ -12,15 +13,15 @@
 
 // VDP specific (for VDU 23,0,n commands)
 //
-#define VDP_gp			 		0x00
-#define VDP_keycode				0x01
-#define VDP_cursor				0x02
-#define VDP_scrchar				0x03
-#define VDP_scrpixel			0x04
-#define VDP_audio				0x05
-#define VDP_mode				0x06
-#define VDP_rtc					0x07
-#define VDP_keystate			0x08
+#define VDP_gp			 		0x80
+#define VDP_keycode				0x81
+#define VDP_cursor				0x82
+#define VDP_scrchar				0x83
+#define VDP_scrpixel			0x84
+#define VDP_audio				0x85
+#define VDP_mode				0x86
+#define VDP_rtc					0x87
+#define VDP_keystate			0x88
 #define VDP_logicalcoords		0xC0
 #define VDP_terminalmode		0xFF
 
