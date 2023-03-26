c57a81c2b9d3b4187320eebfd21affb4d5722fdd
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-26 13:49:17 +0100

    Improved ESP32->eZ80 boot sync

--- main.c
+++ main.c
@@ -21,6 +21,7 @@
  *								+ Stubbed command history
  * 22/03/2023:					+ Moved command history to mos_editor.c
  * 23/03/2023:					+ Increased baud rate to 1152000
+ * 								+ Improved ESP32->eZ80 boot sync
  */
 
 #include <eZ80.h>
@@ -29,6 +30,7 @@
 #include <CTYPE.h>
 #include <String.h>
 
+#include "defines.h"
 #include "config.h"
 #include "uart.h"
 #include "spi.h"
@@ -46,21 +48,42 @@ extern void *	set_vector(unsigned int vector, void(*handler)(void));
 extern void 	vblank_handler(void);
 extern void 	uart0_handler(void);
 
-extern char coldBoot;				// 1 = cold boot, 0 = warm boot
-extern volatile	char keycode;		// Keycode 		
+extern char 			coldBoot;		// 1 = cold boot, 0 = warm boot
+extern volatile	char 	keycode;		// Keycode 
+extern volatile char	gp;				// General poll variable
 
-static FATFS 	fs;					// Handle for the file system
-static char  	cmd[256];			// Array for the command line handler
+static FATFS 	fs;						// Handle for the file system
+static char  	cmd[256];				// Array for the command line handler
 
-// Wait for the ESP32 to respond with an ESC character to signify it is ready
-// Parameters: None
-// Returns: None
+// Wait for the ESP32 to respond with a GP packet to signify it is ready
+// Parameters:
+// - pUART: Pointer to a UART structure
+// - baudRate: Baud rate to initialise UART with
+// Returns:
+// - 1 if the function succeeded, otherwise 0
 //
-void wait_ESP32(void) {
-	char ch = 0; 	
-	while(ch != 27) {
-		ch = getch();
-	}	
+int wait_ESP32(UART * pUART, UINT24 baudRate) {	
+	int	i, t;
+
+	pUART->baudRate = baudRate;			// Initialise the UART object
+	pUART->dataBits = 8;
+	pUART->stopBits = 1;
+	pUART->parity = PAR_NOPARITY;
+	open_UART0(pUART);					// Open the UART 
+	init_timer0(10, 16, 0x00);  		// 10ms timer for delay
+	gp = 0;								// Reset the general poll byte	
+	for(t = 0; t < 4; t++) {			// A timeout loop (4 = 1 second)
+		putch(23);						// Send a general poll packet
+		putch(0);
+		putch(VDP_gp);
+		putch(1);
+		for(i = 0; i < 25; i++) {		// Wait 250ms
+			wait_timer0();
+		}
+		if(gp == 1) break;				// If general poll returned, then exit for loop
+	}
+	enable_timer0(0);					// Disable the timer
+	return gp;
 }
 
 // Initialise the interrupts
@@ -75,21 +98,19 @@ void init_interrupts(void) {
 int main(void) {
 	UART 	pUART;
 
-	pUART.baudRate = 1152000; 						// Max baud rate; previous stable value was 384000 
-	pUART.dataBits = 8;
-	pUART.stopBits = 1;
-	pUART.parity = PAR_NOPARITY;
-
 	DI();											// Ensure interrupts are disabled before we do anything
 	init_interrupts();								// Initialise the interrupt vectors
 	init_rtc();										// Initialise the real time clock
 	init_spi();										// Initialise SPI comms for the SD card interface
 	init_UART0();									// Initialise UART0 for the ESP32 interface
-	open_UART0(&pUART);								// Open the UART 
-	if(coldBoot > 0) {								// If a cold boot has been detected
-		wait_ESP32();								// Wait for the ESP32 to finish its bootup
-	}
-	else {											// Otherwise warm boot,
+	EI();											// Enable the interrupts now
+	
+	if(!wait_ESP32(&pUART, 1152000)) {				// Try to lock onto the ESP32 at maximum rate
+		if(!wait_ESP32(&pUART, 384000))	{			// If that fails, then fallback to the lower baud rate
+			gp = 2;									// Flag GP as 2, just in case we need to handle this error later
+		}
+	}	
+	if(coldBoot == 0) {								// If a warm boot detected then
 		putch(12);									// Clear the screen
 	}
 	printf("Agon Quark MOS Version %d.%02d", MOS_version, MOS_revision);
@@ -97,7 +118,9 @@ int main(void) {
 		printf(" RC%d", MOS_rc);
 	#endif
 	printf("\n\r\n\r");
-	EI();											// Enable the interrupts now
+	#if	DEBUG > 0
+	printf("@Baud Rate: %d\n\r\n\r", pUART.baudRate);
+	#endif
 
 	f_mount(&fs, "", 1);							// Mount the SD card
 
